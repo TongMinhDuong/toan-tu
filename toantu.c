d3a8419e1b516bba0905edb49d6f7a5f832e168b
TongMinhDuong <101384330+TongMinhDuong@users.noreply.github.com>
2022-03-13 10:23:42 +0700

    Add files via upload

---
+++ toantu.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int x,y;
+	x=5;
+	y=2;
+	
+	printf(" so nguyen : %d & %d\n", x,y);
+	printf(" tong : %d\n ", x+y);
+	printf(" hieu : %d\n", x-y);
+	printf(" thuong : %d\n", x/y);
+	printf(" tich : %d\n", x*y);
+	printf(" phan tram : %d", x%y);
+	return 0;
+}
